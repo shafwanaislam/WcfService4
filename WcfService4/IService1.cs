c4bbdcc1208f2c6dd1ff7718383e4c4c1447a624
shanjana <shanjana@DESKTOP-CFTVV8M>
2017-10-23 04:30:49 +0200

    y

--- WcfService4/IService1.cs
+++ WcfService4/IService1.cs
@@ -9,6 +9,8 @@ using System.Text;
 namespace WcfService4
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+
+
     [ServiceContract]
     public interface IService1
     {
